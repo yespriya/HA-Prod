e668ea77407ae3a218094817fa19c82612bd4c54
Prit Tejani <prittejani01@gmail.com>
2026-02-20 21:43:03 +0530

    Fix: Otp redirection to login when invalid otp

--- HelloAlfred/Screens/Authentication/View Model/AuthViewModel.swift
+++ HelloAlfred/Screens/Authentication/View Model/AuthViewModel.swift
@@ -141,11 +141,6 @@ class AuthViewModel {
             .subscribe(onSuccess: { [weak self] response in
                 self?.simpleResponse = response
                 if response.status ?? false == false {
-                    if response.statuscode == 401 {
-                        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-                            appDelegate.redirectToLogin(errorMsg: response.message)
-                        }
-                    }
                     self?.errorMessage = response.message
                     self?.isError = true
                     self?.errorMessageAlert?()
